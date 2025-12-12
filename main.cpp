520c6fb262332d0becaefda359b21309b6268f31
Zawar Fahim <zawar.fahim2004@gmail.com>
2025-12-13 02:12:15 +0500

    libraries

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <limits>
+#include <cctype>
+#include <algorithm>
+#include <cstdlib>
+#include <queue>
+#include <stack>
+#include <iomanip>
+#include <cmath>
 
 using namespace std;
 
